48eabe257f15617d9432f6db4ad7c15a70b58a40
Daniel Fennhagen <fennhagen.daniel@hotmail.com>
2019-04-12 11:03:10 +0200

    db connection setup

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace Server
 {
@@ -10,6 +12,40 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            //MySqlConnection db = new MySqlConnection();
+            MySqlConnection db = ExecuteServer();
+            TestConnection(db);
+
+            Console.ReadLine();
+        }
+        public static MySqlConnection ExecuteServer()
+        { 
+            string connectionString = string.Format("Server=aass.oru.se; database=c5_dt117g_vt19; UID=c5_dt117g_vt19; password=dt117g_vt19");
+            MySqlConnection con = new MySqlConnection(connectionString);
+            return con;
+        }
+
+        public static void TestConnection(MySqlConnection db)
+        {
+            try
+            {
+                db.Open();
+                string query = "SELECT * FROM Daniel";
+                var cdm = new MySqlCommand(query, db);
+                var reader = cdm.ExecuteReader();
+                Console.WriteLine(query);
+
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetString(1));
+                }
+
+                db.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
