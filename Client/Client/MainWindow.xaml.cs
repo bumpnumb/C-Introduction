71df8563c7071d8e44d9a33568561ee5312522d4
Thomas <wobbister@gmail.com>
2019-04-12 13:13:52 +0200

    client socket commit

--- Client/Client/MainWindow.xaml.cs
+++ Client/Client/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Client.services;
 
 namespace Client
 {
@@ -23,6 +24,12 @@ namespace Client
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e) {
+            AsynchronousClient.StartClient();
+            this.Title = "askdndhfjauishi";
         }
     }
 }
