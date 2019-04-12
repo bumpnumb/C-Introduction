2a85390a7a2a9c55cf47e953edb1500898289943
Daniel Fennhagen <fennhagen.daniel@hotmail.com>
2019-04-12 11:24:25 +0200

    Improved reading from db

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -30,22 +30,42 @@ namespace Server
             try
             {
                 db.Open();
+                Console.WriteLine("Connection to db has been made!\n");
                 string query = "SELECT * FROM Daniel";
                 var cdm = new MySqlCommand(query, db);
                 var reader = cdm.ExecuteReader();
-                Console.WriteLine(query);
+
+                Console.WriteLine("Querrying:\n" + query);
 
                 while (reader.Read())
                 {
-                    Console.WriteLine(reader.GetString(1));
+                    person temp = new person();
+                    temp.Name = reader.GetString("Namn");
+                    temp.Betyg = reader.GetString("Betyg");
+                    temp.Yes = reader.GetInt32("Yes");
+                    temp.Bobby = reader.GetString("Bobby_Tables");
+                    temp.print();
                 }
 
                 db.Close();
+                Console.WriteLine("\nClosed db connection!");
             }
             catch (MySqlException ex)
             {
                 Console.WriteLine(ex);
             }
+        }
+    }
+    public class person
+    {
+        public string Name { get; set; }
+        public string Betyg { get; set; }
+        public int Yes { get; set; }
+        public string Bobby { get; set; }
+
+        public void print()
+        {
+            Console.WriteLine(this.Name + ' ' + this.Betyg + ' ' + this.Yes + ' ' + this.Bobby);
         }
     }
 }
