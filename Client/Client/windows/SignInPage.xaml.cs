05015f5aac04d6e766177e0767d121a915071a44
Thomas <wobbister@gmail.com>
2019-05-09 12:31:08 +0200

    Fixed Page switcher

---
+++ Client/Client/windows/SignInPage.xaml.cs
@@ -0,0 +1,124 @@
+﻿using Client.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client.windows
+{
+    /// <summary>
+    /// Interaction logic for SignInPage.xaml
+    /// </summary>
+    public partial class SignInPage : Page
+    {
+        string ID;
+        string PW;
+        public SignInPage() {
+            InitializeComponent();
+            ClientControll newClient = new ClientControll();
+        }
+        public void setTitle(string str) {
+            this.Title = str;
+        }
+
+        private void PwGotFocus(object sender, RoutedEventArgs e) {
+            if (PwBox.Password == "Password")
+                PwBox.Password = "";
+        }
+
+        private void PwLostFocus(object sender, RoutedEventArgs e) {
+            if (PwBox.Password == "") {
+                PwBox.Password = "Password";
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e) {
+            string username = IDBox.Text;
+
+        }
+
+        private void IDBox_GotFocus(object sender, RoutedEventArgs e) {
+            if (IDBox.Text == "Username")
+                IDBox.Text = "";
+        }
+
+        private void IDBox_LostFocus(object sender, RoutedEventArgs e) {
+            if (IDBox.Text == "") {
+                IDBox.Text = "Username";
+            }
+        }
+
+        private void Window_KeyDownEvent(object sender, KeyEventArgs e) {
+            if (IDBox.Text != "" && IDBox.Text != "Username") {
+                if (PwBox.Password != "" && PwBox.Password != "Password") {
+                    if (e.Key == Key.Enter) {
+                        ID = IDBox.Text;
+                        PW = PwBox.Password;
+                        login(ID, PW);
+                        //listen for positive response
+                        openMainProgramWindow();
+                    }
+                }
+
+            }
+        }
+
+        private void signin(object sender, RoutedEventArgs e) {
+            ID = IDBox.Text;
+            PW = PwBox.Password;
+            login(ID, PW);
+            //listen for positive response
+            openMainProgramWindow();
+        }
+
+        private void signupBtn(object sender, MouseButtonEventArgs e) {
+            App.MainWindowRef.Main.Navigate(new SignUpPage());
+            App.MainWindowRef.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            //sng.Top = this.Top;
+            //sng.Left = this.Left;
+            //sng.Height = this.Height;
+            //sng.Width = this.Width;
+
+            //App.Current.MainWindow = sng;
+            //this.Close();
+            //sng.Show();
+        }
+
+        private void login(string id, string pw) {
+            Message loginMsg = new Message();
+            loginMsg.Type = MessageType.Login;
+            loginMsg.Data = id + "=;=" + pw; //DONT FORGET TO ADD RESTRICTIONS TO NAMING
+            ClientControll.Send(loginMsg);
+            /*Fixa en snygg json grej här för ID och PW så det går att ha vilket namn som helst
+            "{
+                'ID': id,
+                'PW': pw
+            }"*/
+
+        }
+
+        public void openMainProgramWindow() {
+
+            App.MainWindowRef.Height = 768;
+            App.MainWindowRef.Width = 1028;
+            App.MainWindowRef.Main.Navigate(new AdminMainPage());
+            App.MainWindowRef.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            //PageHolderWindow mnprg = new PageHolderWindow();
+            //App.Current.MainWindow = mnprg;
+            //this.Close();
+            //mnprg.Show();
+        }
+    }
+}
