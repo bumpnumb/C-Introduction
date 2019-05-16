653a5c61f322c598833f2997028410d216d22c31
Daniel Fennhagen <fennhagen.daniel@hotmail.com>
2019-05-16 09:51:27 +0200

    Extensive clientConnection comments

--- Server/services/clientConnection.cs
+++ Server/services/clientConnection.cs
@@ -25,109 +25,147 @@ namespace Server.services
 
     public class Message
     {
+        //Whenever we recieve a message from client, we parse it as a Message type
+        //A message will have a User, Though is this needed? as we could set this in clienthandler...
+
         public MessageType Type { get; set; }
         public string Data { get; set; }
         private User user { get; set; }
 
         public Response CreateResponse()
         {
+            //Our protocoll is build of a message and a response.
+            //All sent messages expect one response.
             Database db = new Database();
 
+            //Create the response to be sent back
             Response rsp = new Response();
+
+
+            //Switch on MessageType for easy handling
             switch (this.Type)
             {
                 case MessageType.NoType:
+                    //This could be a default but we ended up with having a no_message type.
                     Console.WriteLine("Recieved notype message: " + this.Data);
                     break;
 
                 case MessageType.Login:
+                    //Set the response type for parsing on other end.
                     rsp.Type = MessageType.Login;
-                    string nameLogin = this.Data.Split("=;=")[0];
-                    string passwLogin = this.Data.Split("=;=")[1];
 
-                    rsp.user = db.GetUserByName(nameLogin);
-                    if (rsp.user != null)
+                    //Try to find a user with same name
+                    rsp.user = db.GetUserByName(this.Data.Split("=;=")[0]); //change separator please!
+                    if (rsp.user != null) //this could be made into a one-liner, kept apart for ease of readability
                     {
+                        //user was found, fetch salt and hash
                         string salt = db.GetSaltByID(rsp.user.ID);
                         string hash = db.GetHashByID(rsp.user.ID);
 
-                        if (crypto.AuthenticateLogin(passwLogin, hash, salt) == true)
+
+                        //Authenticate user with salt & hash
+                        if (crypto.AuthenticateLogin(this.Data.Split("=;=")[1], hash, salt) == true)
                         {
-                            Console.WriteLine("Successfull login!");
+                            //return some message for client to continue
                             rsp.Data = "success";
                         }
                         else
                         {
-                            Console.WriteLine("Wrong passsword or username!, try again!");
+                            //this is bad!
+                            //don't tell the client it has a correct username but wrong password.
+                            //this makes bruteforcing easier!
+
+                            //instead send "Failure" or similar
                             rsp.Data = "password";
+                            //or send "no user" since that is already a built in function.
                         }
                     }
                     else
                     {
-                        Console.WriteLine("Wrong passsword or username!, try again!");
                         rsp.Data = "no user";
                     }
                     break;
 
                 case MessageType.Register:
                     rsp.Type = MessageType.Register;
-                    string name = this.Data.Split("=;=")[0];
-                    string passw = this.Data.Split("=;=")[1];
 
-                    rsp.user = db.GetUserByName(name);
+                    //try fo fetch user
+                    rsp.user = db.GetUserByName(this.Data.Split("=;=")[0]); //split on other separator!!
                     if (rsp.user != null)
                     {
-                        Console.WriteLine("User already exists!");
                         rsp.Data = "USER EXISTS!";
                     }
                     else
                     {
-                        User temp = crypto.GenerateSaltHash(passw);
-                        db.RegisterUser(name, temp.Salt, temp.Hash);
-                    }
+                        //Generate some salt and hash
+                        User tempUser = crypto.GenerateSaltHash(this.Data.Split("=;=")[1]); //split on other separator!!
+                        db.RegisterUser(this.Data.Split("=;=")[0], tempUser.Salt, tempUser.Hash);
 
+                        //lets not be done here. as is now, we return nothing.
+                        //from here, do a login atempt, and return thee user.
+                        //Why should I register then login?
+                        //process could be streamlined!
+                    }
                     break;
 
                 case MessageType.Competition:
-
-                    string[] part = this.Data.Split("\r\n");
+                    //split string in case we want to know more
+                    string[] part = this.Data.Split("\r\n"); //correct separator use!
                     List<Competition> comp;
 
+
+                    //Competition messagetypes will be as following:
+
+                    //Type: Competition,
+                    //Data: "prefix \r\n suffix"
+                    //User: dont think this is honestly needed here!
+
+                    //We switch on data.prefix, and is suffix is needed, use that to identify specific competitions.
                     switch (part[0])
                     {
                         case "GetAll":
+                            //fetch all competitions
                             rsp.Type = MessageType.Competition;
                             comp = db.GetAllCompetitions();
 
+                            //Newtonsoft.Json is used to turn objects to nice looking strings
                             rsp.Data = JsonConvert.SerializeObject(comp);
                             break;
 
                         case "GetActive":
+                            //Fetch all competitions which are started but not ended
                             rsp.Type = MessageType.Competition;
                             comp = db.GetActiveCompetitions();
+
                             rsp.Data = JsonConvert.SerializeObject(comp);
                             break;
 
                         case "CreateCompetition":
-                            //Competition CreateComp = CreateCompetition();
-                            //rsp.Data = JsonConvert.SerializeObject(CreateComp);
-
                             rsp.Type = MessageType.Competition;
 
-                            //ha detta som en array med string istället för en string, lista med listor...
-                            /*
-                            string[] allJudges;
-                            string[] Judges = this.Data.Split("=;=")[0];
+                            //this needs a lot of work.
 
-                            foreach (string value in Judges)
-                            {
-                                string[] allJudges = Judges...
-                            }
-                            */
+                            //My thoughts:
+                            //part[0] will be "CreateCompetition"
+                            //part[1] will be some json looking like this:
+                            //{"Name": x,
+                            // "somedata": ifneeded,
+                            // "Judges":[{"ID": y1, "ID": y2, "ID": y3, "ID": y4, "ID": y5 .....}],
+                            // "Divers":[{"ID": y1, "ID": y2, "ID": y3, "ID": y4, "ID": y5 .....}]
+                            //}
+
+                            //send that from client side
+                            //parse part[1] using jsonconvert
+
+                            //send object to function
+                            //function loops thhrough divers and judges pushing querry to db.
+                            
+
 
                             string Judges = this.Data.Split("=;=")[0];    //Hur blir detta om vi har olika många för vaje comp.
                             string Divers = this.Data.Split("=;=")[1];    //samma sak här
                             string NåttMer = this.Data.Split("=;=")[2];
+                            
 
                             if (competitions.createCompetition(Judges, Divers) == true) //== something
                             {
@@ -145,8 +183,7 @@ namespace Server.services
                     break;
             }
 
-            //Send(resp);
-
+            //returns response to be sent
             return rsp;
         }
 
