7a596c5125403ae93ef78d0da74ff930a5e5788a
Nedim Efendic <Nedo98@gmail.com>
2019-05-21 14:17:50 +0200

    75% done with jumpCode function

--- Server/services/Competition.cs
+++ Server/services/Competition.cs
@@ -231,31 +231,91 @@ namespace Server.services
                         }
                     }
                     if (chars[2] == 5){
-                        CodeString += "2 1/2 Somersaults";
+                        CodeString += "2 1/2 Somersaults ";
                         if(chars[3] == 1){
-                            CodeString += "1/2 Twist";
+                            CodeString += "1/2 Twist";  //5151
                         }
                         else if (chars[3] == 2){
-                            CodeString += "1 Twist";
+                            CodeString += "1 Twist";    //5152
                         }
                         else if (chars[3] == 4){
-                            CodeString += "2 Twists";
+                            CodeString += "2 Twists";   //5154
                         }
                         else if (chars[3] == 6){
-                            CodeString += "3 Twist";
+                            CodeString += "3 Twist";    //5156
                         }
                     }
                     if (chars[2] == 7){
-
+                        CodeString += "3 1/2 Somersaults ";
+                        if(chars[3] == 2){
+                            CodeString += "1 Twist";    //5172
+                        }
                     }
                 }
                 if (chars[1] == 2){
+                    CodeString += "Back ";
+                    if(chars[2] == 1){
+                        CodeString += "Dive ";
+                        if(chars[3] == 1){
+                            CodeString += "1/2 Twist";  //5211
+                        }
+                        else if (chars[3] == 1){
+                            CodeString += "1 Twist";    //5212
+                        }
+                    }
+                    else if (chars[2] == 2){
+                        CodeString += "Somersault ";
+                        if(chars[3] == 1){
+                            CodeString += "1/2 Twist";  //5221
+                        }
+                        else if (chars[3] == 2){
+                            CodeString += "1 Twist";    //5222
+                        }
+                        else if (chars[3] == 3){
+                            CodeString += " 1 1/2 Twists";  //5223
+                        }
+                        else if (chars[3] == 5){
+                            CodeString += " 2 1/2 Twists";  //5225
+                        }
+                        else if (chars[3] == 7){
+                            CodeString += " 3 1/2 Twists";  //5227
+                        }
+                    }
+                    else if (chars[2] == 3){
+                        CodeString += "1 1/2 Somersault ";
+                        if(chars[3] == 1){
+                            CodeString += "1/2 Twist";  //5231
+                        }
+                        else if (chars[3] == 1){
+                            CodeString += "1 Twist";    //5232
+                        }
+                        else if (chars[3] == 1){
+                            CodeString += "2 1/2 Twists";    //5235
+                        }
+                        else if (chars[3] == 1){
+                            CodeString += "3 1/2 Twists";    //5237
+                        }
+                        else if (chars[3] == 1){
+                            CodeString += "4 1/2 Twists";    //5239
+                        }
+                    }
+                    else if (chars[2] == 5){
+                        CodeString += "2 1/2 Somersaults ";
+                        if(chars[3] == 1){
+                            CodeString += "1/2 Twist";  //5251
+                        }
+                        else if (chars[3] == 3){
+                            CodeString += "1 1/2 Twists";   //5253
+                        }
+                        else if (chars[3] == 5){
+                            CodeString += "2 1/2 Twists";   //5255
+                        }
+                    }
+                }
+                else if (chars[1] == 3){
 
                 }
-                if (chars[1] == 3){
-
-                }
-                if (chars[1] == 4){
+                else if (chars[1] == 4){
 
                 }
             }
