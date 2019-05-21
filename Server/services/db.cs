e8ce5f3906471245f87971d4c727e621d74f4e93
Nedim Efendic <Nedo98@gmail.com>
2019-05-21 10:26:54 +0200

    min kod syncar inte...

--- Server/services/db.cs
+++ Server/services/db.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Data;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Text;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
@@ -46,13 +45,21 @@ namespace Server.services
                 context.SaveChanges();
             }
         }
-        public User GetUserByID(int ID)
+        public void GetUserByID(int ID)
         {
             using (var context = new DivingCompDbContext())
             {
-                User u = new User();
-                u = context.Users.Where(x => x.ID == ID).FirstOrDefault();
-                return u;
+                var u = context.Users.Where(x => x.ID == ID);
+
+                foreach (var user in u)
+                {
+                    var data = new StringBuilder();
+                    data.AppendLine($"ID: {user.ID}");
+                    data.AppendLine($"Name: {user.Name}");
+                    data.AppendLine($"Salt: {user.Salt}");
+                    data.AppendLine($"Hash: {user.Hash}");
+                    Console.WriteLine(data.ToString());
+                }
             }
         }
         public User GetUserByName(string name)
@@ -98,28 +105,27 @@ namespace Server.services
                 return u.Hash;
             }
         }
-        public Competition CreateCompetition(CompetitionWithUser CompInfo, List<Jump> jumps)
+        public void CreateCompetition(CompetitionWithUser CompInfo)
         {
             var context = new DivingCompDbContext();
             context.Database.EnsureCreated();
 
             Competition c = new Competition();
 
+            c.ID = CompInfo.ID;
             c.Name = CompInfo.Name;
             c.Start = CompInfo.Start;
+            c.Finished = CompInfo.Finished;
             c.Jumps = CompInfo.Jumps;
 
             context.Competitions.Add(c);
 
-
-            List<int> CUIDs = new List<int>();
             foreach (User userJumper in CompInfo.Users)
             {
                 CompetitionUser temp = new CompetitionUser();
                 temp.CID = CompInfo.ID;
                 temp.UID = userJumper.ID;
                 context.CompetitionUsers.Add(temp);
-                CUIDs.Add(temp.ID);
             }
 
             foreach (User userJudge in CompInfo.Judges)
@@ -130,50 +136,9 @@ namespace Server.services
                 context.CompetitionJudges.Add(temp);
             }
 
-            context.SaveChanges();
-
-            Console.WriteLine(c.ID);
-
-            //....................../´¯/)
-            //....................,/¯../ 
-            //.................../..../ 
-            //............./´¯/'...'/´¯¯`·¸ 
-            //........../'/.../..../......./¨¯\ 
-            //........('(...´...´.... ¯~/'...') 
-            //.........\.................'...../ 
-            //..........''...\.......... _.·´ 
-            //............\..............( 
-            //..............\.............\...
-
-
-
-            //public int CUID { get; set; } jump.cuid är just nu en id på en person.
-            //public string Code { get; set; }
-            //public string Name { get; set; }
-            //public float Difficulty { get; set; }
-            //public int Number { get; set; }
-
-
-            foreach (Jump j in jumps)
-            {
-                Jump temp = new Jump();
-                foreach (int ID in CUIDs) //cuid is a compuser ID
-                {
-                    if (ID == j.CUID)// this means our jump belongs to this compuser
-                    {
-                        temp.CUID = ID;
-                        temp.Code = j.Code;
-                        temp.Number = j.Number;
-                        temp.Name = JumpHelper.GenerateJumpNameFromCode(j.Code);
-                        temp.Difficulty = JumpHelper.GenerateJumpDifficultyFromCode(j.Code);
-                        context.Jumps.Add(temp);
-                    }
-                }
-
-            }
+            //Jumps ska bli assignade här till varje user-Jumps
 
             context.SaveChanges();
-            return c;
         }
 
 
@@ -234,42 +199,13 @@ namespace Server.services
             }
         }
 
-        public CompetitionWithUser GetCompetitionWithUserFromID(int ID)
-        {
-            using (var context = new DivingCompDbContext())
-            {
-                Competition comp = context.Competitions.Where(x => x.ID == ID).FirstOrDefault();
-                CompetitionWithUser cwu = new CompetitionWithUser();
-
-                cwu.ID = comp.ID;
-                cwu.Name = comp.Name;
-                cwu.Start = comp.Start;
-                cwu.Finished = comp.Finished;
-                cwu.Jumps = comp.Jumps;
-                List<User> users = context.Users.Where(u => context.CompetitionUsers.Any(cu => u.ID == cu.UID & cu.CID == comp.ID)).ToList();
-                List<User> judges = context.Users.Where(j => context.CompetitionJudges.Any(cj => j.ID == cj.UID & cj.CID == comp.ID)).ToList();
-                cwu.Users = users;
-                cwu.Judges = users;
-                return cwu;
-            }
-        }
         public CompetitionWithResult GetCompetitionWithResultFromID(int ID)
         {
-            using (var context = new DivingCompDbContext())
-            {
-                CompetitionWithResult cwr = new CompetitionWithResult();
-                cwr.Comp = GetCompetitionWithUserFromID(ID);
-
-                //users = context.Users.Where(u => context.CompetitionUsers.Any(cu => u.ID == cu.UID & cu.CID == comp.ID)).ToList();
-                //users where id = ( in competition users where cu.UID <-- and cu.cid == comp.id)
-                cwr.Jumps = context.Jumps.Where(jump =>
-                    context.CompetitionUsers.Any(cu => jump.CUID == cu.ID && cu.CID == cwr.Comp.ID)).ToList();
-
-                cwr.Results = context.Results.Where(res => cwr.Jumps.Any(jump => res.JumpID == jump.ID)).ToList();
 
 
-                return cwr;
-            }
+
+
+            return new CompetitionWithResult();
         }
 
         public void SetScoreToJump(Result ResultInfo)
